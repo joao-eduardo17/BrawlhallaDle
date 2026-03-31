8df51a08e14da0dfdf0e89707590528ff0ddce5c
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2026-03-31 11:00:59 -0300

    Initializng project

---
+++ src/main/java/com/project/brawlhalladle/BrawlhalladleApplication.java
@@ -0,0 +1,13 @@
+package com.project.brawlhalladle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrawlhalladleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrawlhalladleApplication.class, args);
+	}
+
+}
